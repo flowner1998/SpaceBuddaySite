cf21e6ff13818370b76ccb1bd4313235c29897e8
FLORIS-PC <flowner@hotmail.com>
2016-10-14 17:17:45 +0200

    -changes to the navigation bar in "script.js" and "style.css".

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
  * Created by Floris on 14-10-2016.
  */
 $(document).ready(function(){
-    $(".navButton").click(function(){
-        $(this).css("background-color", "8C8C8C");
+    $("#navButton").hover(function(){
+        $(this).css("background-color", "#8C8C8C");
+    },function(){
+        $(this).css("background-color", "#313131");
     });
 });
