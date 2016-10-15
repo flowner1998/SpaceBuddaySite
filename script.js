e08d460d3bb3abcd8d71b8c0332bc47c9364a57e
FLORIS-PC <flowner@hotmail.com>
2016-10-16 00:21:23 +0200

    -fixed navigation buttons

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
  * Created by Floris on 14-10-2016.
  */
 $(document).ready(function(){
-    $("#navButton").hover(function(){
+    $(".navButton").hover(function(){
         $(this).css("background-color", "#8C8C8C");
     },function(){
-        $(this).css("background-color", "#313131");
+        $(this).css("background-color", "#1c6ca6");
     });
 });
