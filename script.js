cade1ab23042ef941131e3bc538d8d35891b3707
FLORIS-PC <flowner@hotmail.com>
2016-10-24 10:27:52 +0200

    -Fixed Scrolling height

--- script.js
+++ script.js
@@ -19,12 +19,12 @@ $(document).ready(function(){
     });
     $("#navLink1").click(function() {
         $('html, body').animate({
-            scrollTop: $("#content1").offset().top - 85
+            scrollTop: $("#content1").offset().top - 40
         }, 1200);
     });
     $("#navLink2").click(function() {
         $('html, body').animate({
-            scrollTop: $("#content2").offset().top - 85
+            scrollTop: $("#content2").offset().top - 40
         }, 1200);
     });
 });
